4772ed6c303878c314b042c461d3037c1c7bed0f
Tracy Steinberg <tracyadelesteinberg@gmail.com>
2017-07-17 22:07:12 +0100

    partial basic homework

--- game.rb
+++ game.rb
@@ -1,6 +1,6 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('./models/rock_paper')
+# require_relative('./models/rock_paper')
 require('json')
 
 get '/' do
@@ -8,24 +8,28 @@ get '/' do
 end
 
 
+# get '/rock-paper-scissors' do
+#     result = 1 + rand(3)
+#     return result.to_s
+# end
+
+
 get '/rock-paper-scissors' do
-    result = 1 + rand(3)
-    return result.to_s
-end
-
-
-get '/all/:num1/:num2/:num3' do
   content_type(:json)
-  winner = RockPaperScissor.new(params[:num1].to_i, params[:num2].to_i, params[:num3].to_i)
+  winner = 1 + rand(3)
+  return winner.to_s
+  
+  # winner = RockPaperScissor.new(params[:num1].to_i, params[:num2].to_i, params[:num3].to_i)
   results=winner
-
-  results=[1, 2, 3]
   
+     if results == '1'
+        puts "You got paper!" 
+     elsif results == '2'
+        puts  "You got scissors"
+     else
+        puts "You got rock - you win!!"
+     end   
   
-  if results==1; puts "You got paper!" end
-  if results==2; puts "You got scissors!" end
-  if results==3; puts "You got rock - you win!!" end
-
   return results.to_json
 end
 
--- models/rock_paper.rb
+++ models/rock_paper.rb
@@ -7,16 +7,4 @@ class RockPaperScissor
   end
 
 
-  def winner(rps)
- 
-          rps=[1, 2, 3]
-        
-  
-          if rps==1; puts "You got paper!" end
-          if rps==2; puts "You got scissors!" end
-          if rps==3; puts "You got rock - you win!!" end
-
-   end
-  
-
 end
